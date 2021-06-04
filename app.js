805510819c0171d11160d698db9d2a3baab9edff
Denis Antonov <antonov25041992@yandex.ru>
2021-06-04 17:38:57 +0300

    feat: add cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
@@ -7,6 +8,25 @@ const router = require('./routes');
 const { limiter } = require('./middlewares/limiter');
 const { centralizedErrorHandler } = require('./middlewares/centralizedErrorHandler');
 const { PORT, MONGO_DB } = require('./utils/constants');
+const CorsError = require('./errors/cors-error');
+
+const whitelist = [
+  'http://localhost:3000',
+  'http://antonov.movies-explorer.nomoredomains.club/',
+  'https://antonov.movies-explorer.nomoredomains.club/',
+];
+const corsOptions = {
+  origin(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new CorsError('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
+  credentials: true,
+};
 
 const app = express();
 
@@ -18,6 +38,7 @@ mongoose.connect(MONGO_DB, {
 });
 
 app.enable('trust proxy');
+app.use(cors(corsOptions));
 app.use(helmet());
 app.use(limiter);
 app.use(express.json());
